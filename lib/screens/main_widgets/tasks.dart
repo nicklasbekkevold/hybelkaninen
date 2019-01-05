4ea96ecca9c892e587ac8aeb597e89e0d4360fe9
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-01-05 13:26:24 +0100

    Fixed scroll issues related to tasks

--- lib/screens/main_widgets/tasks.dart
+++ lib/screens/main_widgets/tasks.dart
@@ -8,7 +8,7 @@ class Tasks extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.symmetric(vertical: 30.0),
-      height: 400.0,
+      height: 380.0,
       child: Card(
         margin: EdgeInsets.all(0.0),
         child: Column(
@@ -20,7 +20,7 @@ class Tasks extends StatelessWidget {
               padding: EdgeInsets.all(15.0),
               child: Text('TODOs: ', style: Theme.of(context).textTheme.body1,)
             ),
-            Divider(),
+            Divider(height: 0.0,),
             Expanded(
               child: StreamBuilder(
                 stream: Firestore.instance.collection('tasks').snapshots(),
@@ -29,6 +29,8 @@ class Tasks extends StatelessWidget {
                     return CircularProgressIndicator();
                   } 
                   return ListView.separated(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
                     separatorBuilder: (context, index) => Divider(height: 1.0, color: Colors.grey,),
                     itemCount: snapshot.data.documents.length,
                     itemBuilder: (context, index) => _buildListItem(context, snapshot.data.documents[index]),
@@ -36,11 +38,13 @@ class Tasks extends StatelessWidget {
                 }
               ),
             ),
-            Divider(),
-            FlatButton(
-              child: Text('SHOW MORE'),
-              textTheme: ButtonTextTheme.accent,
-              onPressed: () => print('Pressed'),
+            Divider(height: 0.0,),
+            Center(
+              child: FlatButton(
+                child: Text('SHOW MORE'),
+                textTheme: ButtonTextTheme.accent,
+                onPressed: () => print('Pressed'),
+              ),
             ),
           ]
         ),
