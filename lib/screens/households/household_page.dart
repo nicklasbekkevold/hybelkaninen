31f134050448f92ce8cea7706e2b36e206b4ad8e
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-03-20 22:11:05 +0100

    Integrate userauth in hpuseholdpage

--- lib/screens/households/household_page.dart
+++ lib/screens/households/household_page.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HouseholdPage extends StatefulWidget {
+
+  final FirebaseUser user;
+
+  HouseholdPage({
+    Key key, 
+    @required this.user
+  }) : super(key: key);
+
   @override
   _HouseholdPageState createState() => _HouseholdPageState();
 }
 
 class _HouseholdPageState extends State<HouseholdPage> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,25 +25,36 @@ class _HouseholdPageState extends State<HouseholdPage> {
           child: Text("My households"),
         ),
       ), 
-      body: GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20.0),
-        crossAxisSpacing: 10.0,
-        crossAxisCount: 2,
-        children: <Widget>[
-          const CircleAvatar(
-            backgroundColor: Colors.amber,
-            child: Text("K"),
-          ),
-          const CircleAvatar(
-            backgroundColor: Colors.amber,
-            child: Text("G"),
-          ),
-          const CircleAvatar(
-            backgroundColor: Colors.amber,
-            child: Icon(Icons.add),
-          ),
-        ],
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: StreamBuilder(
+          stream: Firestore.instance.collection('users').document(widget.user.uid).snapshots(),
+          builder: (context, snapshot) { 
+            if(!snapshot.hasData) { 
+              return LinearProgressIndicator();
+            }
+            return GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20.0),
+              crossAxisSpacing: 10.0,
+              crossAxisCount: 2,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Text("K"),
+                  backgroundColor: Colors.amber,
+                ),
+                CircleAvatar(
+                  child: Text("A"),
+                  backgroundColor: Colors.amber,
+                ),
+                CircleAvatar(
+                  child: Text("H"),
+                  backgroundColor: Colors.amber,
+                )
+              ],
+            ); 
+          } 
+        ),
       ),
     );
   }
